3ca43623e4c131f25e2514bef74ce88d876a7f17
Bonuspunkt <bonuspunkt@gmail.com>
2012-09-19 07:56:36 +0200

    removed obsolete notice

--- index.js
+++ index.js
@@ -1,15 +1,6 @@
-/*
- * ----------------------------------------------------------------------------
- * "THE BEER-WARE LICENSE" (Revision 42):
- * <bonuspunkt@gmail.com> wrote this file. As long as you retain this notice you
- * can do whatever you want with this stuff. If we meet some day, and you think
- * this stuff is worth it, you can buy me a beer in return.
- * ----------------------------------------------------------------------------
- */
-
 module.exports = function(bufferStore) {
-    var length = bufferStore.reduce(function(previous, current) { 
-        return previous + current.length; 
+    var length = bufferStore.reduce(function(previous, current) {
+        return previous + current.length;
     }, 0);
 
     var data = new Buffer(length);
